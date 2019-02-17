0be62508e760c5d62cb45489b4f5b8b3c3b14b21
Yaroslav Sayenko <sayenkoyaroslav@gmail.com>
2019-02-17 18:28:38 +0200

    add edit function, finish

--- src/components/ImageLoad.vue
+++ src/components/ImageLoad.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
-        <h1>{{title}}</h1>
+        <h1>
+            <template  v-if="selectedImage.hasOwnProperty('index')">Edit Image</template>
+            <template  v-else>Upload Image</template>
+        </h1>
         <form @submit.prevent = "submitForm">
+            <label class = "label" for = "tooltip" v-show = "!form.tooltip">Set tooltip</label>
             <input
-                    id="exampleInput2"
+                    id="tooltip"
                     type="text"
                     v-model="form.tooltip"
-                    required
                     class="form-control"
                     placeholder="Enter tooltip"
             />
+            <label class = "label" for = "file" v-show = "!form.imageData">Choose image</label>
             <input
                     @change="previewImage"
                     accept="image/*"
@@ -17,30 +21,32 @@
                     type="file"
                     id="file"
                     class="form-control-file"
-                    required
             />
 
-            <button type="submit" class = "btn btn-primary">Submit</button>
+            <div class = "label" v-show = "form.imageData.length > 0">Click on the image to set tooltip position</div>
+            <div class="image-preview" v-if="form.imageData.length > 0">
+                <img
+                        class="preview"
+                        :src="form.imageData"
+                        @click = "setTooltipPosition"
+                        ref = "image"
+                >
+                <div class = "image-tooltip" v-if = "form.tooltip" :style = "tooltipPosition">
+                    {{form.tooltip}}
+                </div>
+            </div>
+            <div v-if = "form.imgName">{{form.imgName}}</div>
+
+            <button type="submit" class = "btn btn-primary" :disabled="!isFormFilled">Submit</button>
+            <button @click = "cancelForm" type="reset" v-if = "isFormNotEmpty" class = "btn btn-danger cancel">Cancel</button>
 
         </form>
 
-        <h3 v-if = "form.imageData && form.tooltip">Click on the image to set tooltip position</h3>
-        <div class="image-preview" v-if="form.imageData.length > 0">
-            <img
-                    class="preview"
-                    :src="form.imageData"
-                    @click = "setTooltipPosition"
-                    ref = "image"
-            >
-            <div class = "image-tooltip" v-if = "form.tooltip" :style = "tooltipPosition">
-                {{form.tooltip}}
-            </div>
-        </div>
+
     </div>
 </template>
 
 <script>
-    /* eslint-disable */
     export default {
         name: "image-load",
         data() {
@@ -52,21 +58,24 @@
                     tooltipX: 0,
                     tooltipY: 0
                 },
-
-                title: 'Upload image'
+            }
+        },
+        props: {
+            selectedImage: {
+                type: Object,
+                default() {
+                    return {};
+                }
             }
         },
         methods: {
             submitForm() {
-                if(this.form.tooltipX && this.form.tooltipY) {
-                    this.$emit('getImages', Object.assign({}, this.form));
-                    this.form.tooltip = '';
-                    this.form.imageData = '';
-                    this.form.tooltipX = 0;
-                    this.form.tooltipY = 0;
-                } else {
-                    console.log('set position')
-                }
+                this.$emit('getImages', Object.assign({}, this.form));
+                this.cancelForm();
+
+            },
+            setImageToEdit() {
+                this.form = Object.assign({}, this.selectedImage.image);
             },
             previewImage(event) {
                 let input = event.target;
@@ -84,6 +93,14 @@
                 let imageHeight = this.$refs.image.clientHeight;
                 this.form.tooltipX = event.offsetX/(imageWidth/100);
                 this.form.tooltipY = event.offsetY/(imageHeight/100);
+            },
+            cancelForm() {
+                this.form.tooltip = '';
+                this.form.imageData = '';
+                this.form.tooltipX = 0;
+                this.form.tooltipY = 0;
+                this.form.imgName = '';
+                this.$emit('deleteSelectedImage');
             }
         },
         computed: {
@@ -92,17 +109,44 @@
                     left: this.form.tooltipX + '%',
                     top: this.form.tooltipY + '%',
                 }
-            }
-        }
+            },
+            isFormNotEmpty() {
+                for(let i in Object.keys(this.form)) {
+                    if(this.form[Object.keys(this.form)[i]]) {
+                        return true;
+                    }
+                }
+                return false;
+            },
+            isFormFilled() {
+                for(let i in Object.keys(this.form)) {
+                    if(this.form[Object.keys(this.form)[i]]) {
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+                return true;
+            },
+
+        },
 
     }
 </script>
 
 <style scoped>
+    form {
+        max-width: 80%;
+        margin: 0 auto;
+    }
     .image-preview {
         width: fit-content;
         margin: 15px auto;
         position: relative;
+        max-width: 100%;
+    }
+    .image-preview img {
+        width: 100%;
     }
     .image-tooltip {
         position: absolute;
@@ -113,19 +157,41 @@
         display: none;
         transform: translate(-50%, -50%);
     }
-    /*.image-tooltip::after {*/
-        /*content: '';*/
-        /*position: absolute;*/
-        /*left: 50%;*/
-        /*transform: translateX(-50%);*/
-        /*top: -30px;*/
-        /*border: 10px solid transparent;*/
-        /*border-bottom: 20px solid rgba(0, 0, 0, 0.5);*/
-    /*}*/
     .image-preview img:hover {
         cursor: pointer;
     }
     .image-preview:hover .image-tooltip {
         display: block;
+    }
+    .label {
+        font-size: 20px;
+        color: blue;
+        margin-top: 15px;
+    }
+    input[type=file] {
+        cursor: pointer;
+        width: 180px;
+        height: 34px;
+        overflow: hidden;
+        margin: 15px auto;
+    }
+    input[type=file]:before {
+        width: 100%;
+        height: 32px;
+        font-size: 16px;
+        line-height: 32px;
+        content: 'Select your file';
+        display: inline-block;
+        background: white;
+        border: 1px solid #000;
+        padding: 0 10px;
+        text-align: center;
+    }
+
+    input[type=file]::-webkit-file-upload-button {
+        visibility: hidden;
+    }
+    .cancel {
+        margin-left: 10px;
     }
 </style>
--- src/components/Images.vue
+++ src/components/Images.vue
@@ -1,18 +1,26 @@
 <template>
     <div>
+        <div v-if = "error" class = "error">{{error}}</div>
         <image-load
             @getImages = "getImages"
+            @deleteSelectedImage = "deleteSelectedImage"
+            :selectedImage = "selectedImage"
+            ref = "imageLoad"
         ></image-load>
 
-        <div class = "">
-            <h1>Images</h1>
+        <div v-if = "images.length">
+            <h1>Uploaded Images</h1>
             <div class = "images">
                 <div
                         class = "images__image"
                         v-for = "(image, index) in images"
                         :key = "index"
                 >
-                    <img @click = "chooseImageToEdit(image, index)" :src="image.imageData"/>
+                    <img :src="image.imageData"/>
+                    <div class = "image__buttons">
+                        <button class = "btn btn-danger" @click = "deleteImage(index)">Delete</button>
+                        <button class = "btn btn-success" @click = "chooseImageToEdit(image, index)">Edit</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -28,16 +36,44 @@
         name: "images",
         data() {
             return{
-                images: []
+                images: [],
+                selectedImage: {
+                },
+                error: ''
             }
         },
         methods: {
             getImages(form) {
-                this.images.push(form)
+                if(this.selectedImage.hasOwnProperty('index')) {
+                    this.images[this.selectedImage.index] = Object.assign({}, form);
+                    this.selectedImage = {}
+                    this.setToLocalStorage();
+                } else {
+                    this.images.push(form);
+                    this.setToLocalStorage();
+                }
+
+            },
+            setToLocalStorage() {
                 localStorage.setItem('images', JSON.stringify(this.images))
             },
             chooseImageToEdit(image, index) {
-                console.log(image, index)
+                this.selectedImage.index = index;
+                this.selectedImage.image = image;
+                this.$refs.imageLoad.setImageToEdit();
+            },
+            deleteImage(index) {
+                if(index === this.selectedImage.index) {
+                    this.error = "You can't delete image which is editing now";
+                    setTimeout(()=> this.error = '', 5000);
+                } else {
+                    this.images.splice(index, 1);
+                    this.setToLocalStorage();
+                }
+
+            },
+            deleteSelectedImage() {
+                this.selectedImage = {}
             }
         },
         mounted() {
@@ -65,8 +101,35 @@
         height: auto;
         overflow: hidden;
         cursor: pointer;
+        position: relative;
+        margin: 5px;
     }
     .images__image img {
         width: 100%;
+    }
+    .image__buttons {
+        position: absolute;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        display: none;
+        width: 100%;
+        height: 100%;
+        top:0;
+    }
+    .images__image:hover .image__buttons {
+        display: flex;
+    }
+    .image__buttons .btn-success {
+        margin-top: 10px;
+    }
+    .error {
+        position: fixed;
+        right: 15px;
+        top: 15px;
+        background: red;
+        color: white;
+        padding: 10px;
+        z-index: 999;
     }
 </style>
